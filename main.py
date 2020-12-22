62c9c6a3da5996508b57e695fd7b03bdc663d49f
chris <directive0@gmail.com>
2020-12-22 15:04:43 -0500

    plasma

--- main.py
+++ main.py
@@ -156,9 +156,9 @@ class Plasma:
 			warp = math.sin(i*10+self.tick)
 
 			# following attempts to fade each element of the pixels colour evenly (needs work)
-		 	colour = abs(int(translate(warp, -1, 1, 0, 255)))
+			colour = abs(int(translate(warp, -1, 1, 0, 255)))
 			pixels[i] = (colour,colour,colour)
-			
+
 		self.tick += self.increment
 
 # recursive function to find an unoccupied place on the strip
